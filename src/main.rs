07da889a921592036d78b90884e6e1dbd0b79591
Alexandroskw <filipbilly.120@gmail.com>
2024-08-17 22:09:34 -0600

    ```main.rs``` reduced

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,12 @@
-use postgres::{Client, Error, NoTls};
+mod db;
 
-fn enable_connection() -> Result<Client, Error> {
-    let conn_string = "host=localhost user=alex_desk password=alex dbname=pokedex";
-    Client::connect(conn_string, NoTls)
-}
+use db::pokemon_tables::create_tables;
+use pokedb::db::connection::enable_connection;
 
 fn main() -> Result<(), postgres::Error> {
     let mut client = enable_connection()?;
-    client.batch_execute(
-        "
-            CREATE TABLE IF NOT EXISTS pokemon (
-                id      INTEGER PRIMARY KEY,
-                pokedex_number INTEGER UNIQUE NOT NULL,
-                name    VARCHAR(100) NOT NULL,
-                height  DECIMAL(5,2)
-        )",
-    )?;
 
-    println!("Table created.");
+    create_tables(&mut client)?;
 
     Ok(())
 }
