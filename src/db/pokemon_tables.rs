249c7bc5f31d0db545bb1f5cab773adcc751fb0c
Alexandroskw <filipbilly.120@gmail.com>
2024-08-18 00:01:35 -0600

    Dropping the tables if exists and creating again

--- src/db/pokemon_tables.rs
+++ src/db/pokemon_tables.rs
@@ -2,10 +2,17 @@ use postgres::{Client, Error};
 
 // Queries are created here
 // Borrowing the client from the established connection
-pub fn create_tables(client: &mut Client) -> Result<(), Error> {
+fn create_pokemon_tables(client: &mut Client) -> Result<(), Error> {
     // Query to create tables into the db
     client.batch_execute(
         "
+            -- Droping tables if exists
+            DROP TABLE IF EXISTS pokemon;
+            DROP TABLE IF EXISTS types;
+            DROP TABLE IF EXISTS pokemon_types;
+            DROP TABLE IF EXISTS abilities;
+            DROP TABLE IF EXISTS pokemon_abilities;
+
             -- Pokemon principle table
             CREATE TABLE IF NOT EXISTS pokemon (
                 id   SERIAL PRIMARY KEY,
@@ -21,7 +28,7 @@ pub fn create_tables(client: &mut Client) -> Result<(), Error> {
                 speed INTEGER
         );
             -- Pokemon types table
-            CREATE IF NOT EXISTS types (
+            CREATE TABLE IF NOT EXISTS types (
                 id SERIAL PRIMARY KEY,
                 name VARCHAR(20)
         );
@@ -29,6 +36,15 @@ pub fn create_tables(client: &mut Client) -> Result<(), Error> {
     )?;
 
     println!("Tables created.");
+
+    Ok(())
+}
+
+// Initialize the pokemon database
+pub fn init_pokemon_database(client: &mut Client) -> Result<(), Error> {
+    // Confirms the creation of the tables in the db
+    create_pokemon_tables(client)?;
+    println!("Pokemon database initalized successful");
 
     Ok(())
 }
