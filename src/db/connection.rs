f86a8757e0791594b9259ed44bfdaf08ef7c439f
Alexandroskw <filipbilly.120@gmail.com>
2024-08-20 00:18:05 -0600

    Confirms the URL from the ```.env``` file

--- src/db/connection.rs
+++ src/db/connection.rs
@@ -1,7 +1,16 @@
+use dotenv::dotenv;
 use postgres::{Client, Error, NoTls};
+use std::env;
 
 // Connecting to the database
 pub fn enable_connection() -> Result<Client, Error> {
-    let conn_string = "host=localhost user=alex_desk password=alex dbname=pokedex";
-    Client::connect(conn_string, NoTls)
+    // Load environment variables from the .env
+    dotenv().ok();
+
+    // Get the URL from the environment variable
+    let database_url =
+        env::var("DATABASE_URL").expect("DATABASE_URL must be set in the .env file. Denied");
+
+    // Establishing connection
+    Client::connect(&database_url, NoTls)
 }
