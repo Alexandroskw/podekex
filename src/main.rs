7425449dd520671a0bf7f901fd472ce349800c86
Alexandroskw <filipbilly.120@gmail.com>
2024-08-20 00:19:22 -0600

    adding the new modules and setting up the ```.env``` file

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,16 @@
 mod db;
+mod users;
 
 use db::pokemon_tables::init_pokemon_database;
 use pokedb::db::connection::enable_connection;
+use users::user_config::setup_env_file;
 
 fn main() -> Result<(), postgres::Error> {
+    match setup_env_file() {
+        Ok(_) => println!("Configuration complete."),
+        Err(e) => eprintln!("Error to create .env file: {}", e),
+    }
+
     let mut client = enable_connection()?;
 
     init_pokemon_database(&mut client)?;
