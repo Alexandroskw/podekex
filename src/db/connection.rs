f80b2475a96710caee4333bb9e9354bc7f78d2ae
Alexandroskw <filipbilly.120@gmail.com>
2024-08-20 21:38:52 -0600

    Little changes and new comments

--- src/db/connection.rs
+++ src/db/connection.rs
@@ -1,16 +1,17 @@
-use crate::db::pokemon_tables::create_pokemon_tables;
+use crate::{db::pokemon_tables::create_pokemon_tables, users::user_config::setup_env_file};
 use dotenv::dotenv;
 use postgres::{Client, Error, NoTls};
 use reqwest::Client as ReqwestClient;
 use std::env;
 
-//Struct for the Pokemon API
+// Struct for the Pokemon API and the client of the DB
 pub struct AppConfig {
     pub db_client: Client,
     pub api_client: ReqwestClient,
     pub api_base_url: String,
 }
 
+// Encapsulated all the configurations
 impl AppConfig {
     pub fn new() -> Result<Self, Box<dyn std::error::Error>> {
         // Load environment variables from the .env
@@ -29,6 +30,12 @@ impl AppConfig {
     pub fn init_database(&mut self) -> Result<(), Error> {
         create_pokemon_tables(&mut self.db_client)
     }
+
+    pub fn setup_env() -> Result<(), Box<dyn std::error::Error>> {
+        setup_env_file()?;
+
+        Ok(())
+    }
 }
 
 // Connecting to the database
@@ -43,7 +50,7 @@ fn enable_connection() -> Result<Client, Error> {
 
 fn config_pokemon_api() -> Result<(ReqwestClient, String), Box<dyn std::error::Error>> {
     let api_base_url =
-        env::var("POKEMON_BASE_API_URL").expect("POKEMON_BASE_API_URL must be set in .env file");
+        env::var("POKEMON_API_BASE_URL").expect("POKEMON_API_BASE_URL must be set in .env file");
 
     let client = ReqwestClient::new();
 
