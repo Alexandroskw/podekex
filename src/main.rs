3888531a6c1063c800ff20d1a62ac7681aff4248
Alexandroskw <filipbilly.120@gmail.com>
2024-08-28 14:33:09 -0600

    Removing the incorrect import to set the ```.env``` file `AppConfig` wasn't the correct import to configure the credentials in the `.env` file.

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,20 @@
 mod db;
 mod users;
 
-use pokedb::db::connection::AppConfig;
+use pokedb::users::user_config::setup_env_file;
 use std::path::Path;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    /*If the '.env' file doesn't exists, the Pokedex will send the user to configure it's
+    credentials. But if the .'env' exists, the pokedex will send a message that everything is
+    correct and the pokedex has been init*/
     if !Path::new(".env").exists() {
         println!(".env not found. Insert your data");
+        // Here the user will set the correct data for the db
+        setup_env_file()?;
     } else {
         println!(".env found. Wait");
     }
-
-    let mut config = AppConfig::new()?;
-    config.init_database()?;
 
     println!("Pokedex started.");
 
