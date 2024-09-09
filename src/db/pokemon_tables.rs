f356787cb7ed732ffcf6162bea795781dbf78045
Alexandroskw <filipbilly.120@gmail.com>
2024-09-08 19:53:42 -0600

    Pushing again for avoiding some issues

---
+++ src/db/pokemon_tables.rs
@@ -0,0 +1,94 @@
+use postgres::{Client, Error};
+use serde_json::Value;
+
+// Queries are created here
+// Borrowing the client from the established connection
+pub fn create_pokemon_tables(client: &mut Client) -> Result<(), Error> {
+    // Query to create tables into the db
+    client.batch_execute(
+        "
+            -- Pokemon principle table
+            CREATE TABLE IF NOT EXISTS pokemon (
+                id   SERIAL PRIMARY KEY,
+                random_id   SERIAL UNIQUE NOT NULL,
+                pokedex_number INTEGER UNIQUE NOT NULL,
+                name    VARCHAR(100) NOT NULL,
+                height  DECIMAL(5,2),
+                weight  DECIMAL(5,2),
+                hp INTEGER,
+                attack INTEGER,
+                defense INTEGER,
+                special_attack INTEGER,
+                special_defense INTEGER,
+                speed INTEGER
+        );
+            -- Types of pokemon table
+            CREATE TABLE IF NOT EXISTS types (
+                id SERIAL PRIMARY KEY,
+                name VARCHAR(20)
+        );
+            -- Pokemon types table
+            CREATE TABLE IF NOT EXISTS pokemon_types (
+                pokemon_id SERIAL REFERENCES pokemon(id),
+                type_id INTEGER REFERENCES types(id),
+                PRIMARY KEY (pokemon_id, type_id)
+        );
+            -- Abilities of pokemon table
+            CREATE TABLE IF NOT EXISTS abilities (
+                id SERIAL PRIMARY KEY,
+                name VARCHAR(50) UNIQUE NOT NULL
+        );
+            -- Abilities from each pokemon table
+            CREATE TABLE IF NOT EXISTS pokemon_abilities (
+                pokemon_id INTEGER REFERENCES pokemon(id),
+                ability_id INTEGER REFERENCES abilities(id),
+                is_hidden BOOLEAN NOT NULL,
+                PRIMARY KEY (pokemon_id, ability_id)
+        );
+
+        ",
+    )?;
+
+    println!("Tables created or uploaded.");
+
+    Ok(())
+}
+
+// Query for inserting the fetching pokemons
+pub fn insert_pokemon_data(
+    client: &mut Client,
+    pokemon_data: &Value,
+) -> Result<(), Box<dyn std::error::Error>> {
+    // Obtain the first data from the API as JSON
+    let pokedex_number = pokemon_data["id"].as_i64().ok_or("Missed")? as i32;
+    let name = pokemon_data["name"].as_str().ok_or("Missed")?.to_string();
+    let height = pokemon_data["height"].as_f64().ok_or("Missed")? / 10.0;
+    let weight = pokemon_data["weight"].as_f64().ok_or("Missed")? / 10.0;
+
+    let stats = pokemon_data["stats"].as_array().ok_or("Missed")?;
+    let hp = stats[0]["base_stat"].as_i64().ok_or("Missed")? as i32;
+    let attack = stats[1]["base_stat"].as_i64().ok_or("Missed")? as i32;
+    let defense = stats[2]["base_stat"].as_i64().ok_or("Missed")? as i32;
+    let special_attack = stats[3]["base_stat"].as_i64().ok_or("Missed")? as i32;
+    let special_defense = stats[4]["base_stat"].as_i64().ok_or("Missed")? as i32;
+    let speed = stats[5]["base_stat"].as_i64().ok_or("Missed")? as i32;
+
+    client.execute(
+        "INSERT INTO pokemon (pokedex_number, name, height, weight, hp, attack, defense, special_attack, special_defense, speed)
+        VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)",
+        &[
+            &pokedex_number,
+            &name,
+            &(height as f32),
+            &(weight as f32),
+            &hp,
+            &attack,
+            &defense,
+            &special_attack,
+            &special_defense,
+            &speed
+        ],
+        )?;
+
+    Ok(())
+}
