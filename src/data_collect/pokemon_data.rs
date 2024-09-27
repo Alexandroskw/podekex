8f4e280be8c7b8d7615c7758e5f4e05764e5db71
Alexandroskw <filipbilly.120@gmail.com>
2024-09-26 20:42:53 -0600

    Fixing the last missmatch types Now the correlation are printing in the screen

--- src/data_collect/pokemon_data.rs
+++ src/data_collect/pokemon_data.rs
@@ -318,11 +318,15 @@ fn correlation_analysis(df: &DataFrame) -> Result<(), Box<dyn Error>> {
         .into_iter()
         .filter_map(|h| h.and_then(|s| s.parse::<f64>().ok()))
         .collect();
+    let weight_f64: Vec<f64> = weight
+        .into_iter()
+        .filter_map(|w| w.and_then(|s| s.parse::<f64>().ok()))
+        .collect();
 
     // Calculating the middle of the columns
     let hp_mean = hp.mean().unwrap();
     let height_mean = height_f64.iter().sum::<f64>() / height_f64.len() as f64;
-    let weight_mean = weight.mean().unwrap();
+    let weight_mean = weight_f64.iter().sum::<f64>() / weight_f64.len() as f64;
 
     let mut covariance_hp_height: f64 = 0.0;
     let mut covariance_hp_weight: f64 = 0.0;
@@ -333,19 +337,25 @@ fn correlation_analysis(df: &DataFrame) -> Result<(), Box<dyn Error>> {
     for ((h, ht), w) in hp
         .into_iter()
         .zip(height_f64.into_iter())
-        .zip(weight.into_no_null_iter())
+        .zip(weight_f64.into_iter())
     {
-        let h_f64 = h as f64;
-        covariance_hp_height += (h_f64 - hp_mean) * (ht - height_mean);
-        covariance_hp_weight += (h_f64 - hp_mean) * (w - weight_mean);
-        hp_variance += (h_f64 - hp_mean).powi(2);
-        h_variance += (ht - height_mean).powi(2);
-        w_variance += (w - weight_mean).powi(2);
+        if let Some(h) = h {
+            let h_f64 = h as f64;
+            covariance_hp_height += (h_f64 - hp_mean) * (ht - height_mean);
+            covariance_hp_weight += (h_f64 - hp_mean) * (w - weight_mean);
+            hp_variance += (h_f64 - hp_mean).powi(2);
+            h_variance += (ht - height_mean).powi(2);
+            w_variance += (w - weight_mean).powi(2);
+        }
     }
 
     let correlation_hp_height = covariance_hp_height / (hp_variance.sqrt() * h_variance.sqrt());
     let correlation_hp_weight = covariance_hp_weight / (hp_variance.sqrt() * w_variance.sqrt());
-    println!("Correlation {correlation_hp_height}, {correlation_hp_weight}");
+    println!(
+        "Correlations
+            hp vs height: {correlation_hp_height}
+            hp vs weight: {correlation_hp_weight}"
+    );
 
     Ok(())
 }
